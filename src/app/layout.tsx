d43512b7307c7ab092c0c85ef27f48ec644aede3
christian-22 <71905486+christian-22@users.noreply.github.com>
2025-05-02 22:20:11 +0000

    adding basic navbar and adjusting styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from "next";
 import { Noto_Sans_Display } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
 
 const notoSansDisplay = Noto_Sans_Display({
   variable: "--font-geist-sans",
@@ -19,9 +32,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${notoSansDisplay.variable} antialiased`}
-      >
+      <body className={`${notoSansDisplay.variable} antialiased bg-black`}>
+        <NavigationMenu className="">
+          <NavigationMenuList className="flex flex-nowrap w-screen m-1 gap-9">
+            <NavigationMenuItem>
+              <Link href="" passHref>
+                <NavigationMenuLink className="text-white">
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="" passHref>
+                <NavigationMenuLink className="text-white">
+                  About
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="" passHref>
+                <NavigationMenuLink className="text-white">
+                  Resumé
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="" passHref>
+                <NavigationMenuLink className="text-white">
+                  Github
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
         {children}
       </body>
     </html>
